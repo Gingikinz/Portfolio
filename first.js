e43db6946436464d5e00fb8c9cdb292ada0d64e0
Gingikinz <spencerjholt@outlook.com>
2018-10-09 11:02:45 -0600

    more js

--- first.js
+++ first.js
@@ -5,3 +5,13 @@ let favoriteSciFi = {
     potato: "Russet",
     number: 3,
 }
+let name = document.querySelector("#name")
+
+fetch('https://swapi.co/api/starships/12')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+      name.textContent = myJason.name;
+    console.log(myJson.name);
+  });
