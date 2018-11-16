db6c2173664118cfc108ddc5741a655e3d423728
Spencer <spencerjholt@outlook.com>
2018-11-16 00:12:52 -0700

    almost flipping?

--- star.js
+++ star.js
@@ -5,15 +5,15 @@ console.log(starships)
 
 const shipContainer = document.querySelector('#container')
 
-var count = 0
-var i = 0
-while (i <= 25) {
-    console.log(starships[count].name)
-    count++
-    i++
-}
+// var count = 0
+// var i = 0
+// while (i <= 25) {
+//     console.log(starships[count].name)
+//     count++
+//     i++
+// }
 
-
+let images = (
 starships.forEach(element => {
     let imgName =`${element.name}`
     let fig = document.createElement('figure')
@@ -26,23 +26,53 @@ starships.forEach(element => {
     shipContainer.appendChild(fig)
     //console.log(imgName)
     
-});
+}));
 
 
 //card back
 
-// starships.forEach(element => {
-//     let stats = {
-//         shipModel: element.model,
-//         shipClass: element.starship_class,
-//         shipCrew: element.crew,
-//         shipDrive: element.hyperdrive_rating
-//     }
-//     let statsList = document.createElement('backList')
-//     // console.log(stats)
-//     statsList.appendChild(stats)
-// })
+const stats = (
+    starships.forEach(element => {
+    let statsContainer = document.createElement('div')
+    statsContainer.className = "figure-back"
+    let shipName = document.createElement('h2')
+    let shipModel = document.createElement('p')
+    let shipClass = document.createElement('p')
+    let shipCrew = document.createElement('p')
+    let shipDrive = document.createElement('p')
+    shipName.textContent = element.name
+    shipModel.textContent = element.model
+    shipClass.textContent = element.starship_class
+    shipCrew.textContent = element.crew
+    shipDrive.textContent = element.hyperdrive_rating
+    statsContainer.appendChild(shipName)
+    statsContainer.appendChild(shipModel)
+    statsContainer.appendChild(shipClass)
+    statsContainer.appendChild(shipCrew)
+    statsContainer.appendChild(shipDrive)
+    shipContainer.appendChild(statsContainer)
+    
+    return statsContainer
+    }));
+    console.log
+    console.log(stats)
 
-var cardflip = document.querySelector('#figure');
-cardflip.addEventListener('click', () => { cardflip.classList.toggle('flipped');
-});
+    // let statsList = document.createElement('backList')
+    // // console.log(stats)
+    // statsList.appendChild(stats)
+
+
+// var cardflip = document.querySelector('#figure');
+// cardflip.addEventListener('click', () => { cardflip.classList.toggle('flipped');
+// });
+
+// starships.forEach(ship => {
+//     let card = document.createElement('div')
+//     card.className = "card"
+//     card.appendChild(images(ship))
+//     card.appendChild(stats(ship))
+//     shipContainer.appendChild(card)
+//     card.addEventListener( 'click', function () {
+//         card.classList.toggle('flipped')
+//     })
+// })
