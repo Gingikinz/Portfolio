9cefe408892b3a2269a8f5dbeaa0859e2a6cc417
Gingikinz <spencerjholt@outlook.com>
2018-11-27 11:06:42 -0700

    Figuring the border

--- star.js
+++ star.js
@@ -4,13 +4,12 @@ console.log(starships)
 //console.log(starships[0].name);
 
 const shipContainer = document.querySelector('#container')
-//const statsContainer = document.querySelector('#scontainer')
 
 starships.forEach(starship => {
 let card = document.createElement('div')
 card.className = 'card'
-card.addEventListener( 'click', () =>  {
-    card.classList.toggle('flipped');})
+    card.addEventListener( 'click', () =>  {
+        card.classList.toggle('flipped');})
 let fig = document.createElement('figure')
 fig.className = 'card__face card__face--front'
 let cap = document.createElement('figcaption')
@@ -22,7 +21,7 @@ fig.appendChild(cap)
 
 card.appendChild(fig)
 
-let back = document.createElement('figure')
+let back = document.createElement('div')
 back.className = 'card__face card__face--back'
 let shipName = document.createElement('h2')
 let shipModel = document.createElement('p')
@@ -43,47 +42,35 @@ back.appendChild(shipDrive)
 card.appendChild(back)
 shipContainer.appendChild(card)
 
-
-
-    
- 
 });
 
+function spaceship(name, model, ship_class, crew, hdr){
+    this.name = name
+    this.model = model
+    this.class = ship_class
+    this.crew = crew
+    this.hdr = hdr
+}
 
-//     starships.forEach(starship => {
-//     let back = document.createElement('figure')
-//     let shipName = document.createElement('h2')
-//     let shipModel = document.createElement('p')
-//     let shipClass = document.createElement('p')
-//     let shipCrew = document.createElement('p')
-//     let shipDrive = document.createElement('p')
-//     shipName.textContent = starship.name
-//     shipModel.textContent = `Model: ${starship.model}`
-//     shipClass.textContent = `Class: ${starship.starship_class}`
-//     shipCrew.textContent = `Crew: ${starship.crew}`
-//     shipDrive.textContent = `Hyperdrive rating: ${starship.hyperdrive_rating}`
-//     back.appendChild(shipName)
-//     back.appendChild(shipModel)
-//     back.appendChild(shipClass)
-//     back.appendChild(shipCrew)
-//     back.appendChild(shipDrive)
+let enterprise = new spaceship("Enterprise", "Constitution", "Cruiser", "250", "<10")
+
+console.log(enterprise)
+
+let button = document.querySelector('#newCardButton')
+button.addEventListener('click', () => {
+    let potato = document.createElement('div')
     
-//     //statsContainer.appendChild(back)
-
-//     shipContainer.appendChild(back) /*Works*/
-// //shipContainer.appendChild(statsContainer)
-// //console.log(statsContainer)
-// });
-
-//console.log(starships.name)
+})
 
 
-    // let statsList = document.createElement('backList')
-    // // console.log(stats)
-    // statsList.appendChild(stats)
 
- 
-// var flip = document.querySelector('figure');
-// flip.addEventListener( 'click', function() {
-//     flip.classList.toggle('flipped');
-// });
+/* example construtor function: */
+// function Bird() {
+//     this.name = "derp";
+//     this.color = "red";
+//     this.can_fly = false;
+// };
+
+// let derp = new Bird()
+
+
